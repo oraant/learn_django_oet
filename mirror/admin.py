44fde7964ec3de5b52ff90b0e9ed6bf747f8a816
oraant <oraant777@gmail.com>
2016-08-22 14:37:15 +0800

    complete models and admin for mirror!

--- mirror/admin.py
+++ mirror/admin.py
@@ -34,21 +34,24 @@ class TableSQLAdmin(admin.ModelAdmin):
             'Cache SQL in MySQL Server', {
                 'description': """
                     <h1> Please Notice That: </h1>
-                    1，添加新表前一定要先测试，测试成功后再确认添加！
-                    <br>
-                    2，SQL语句的结尾不要添加分号！
-                    <br>
-                    3，特殊字符#尽量去掉！
-                    <br>
-                    4，用&lt;TABLE_NAME&gt;来代替不确定的表名！
-                    <br>
-                    5，Create语句必须加上 'if not exists'！
-                    <br>
-                    6，Create必须加上 'ENGINE = MEMORY！'！
-                    <br>
-                    7，Oracle中的Number类型，对应MySQL中的Bigint类型！
-                    <br>
-                    8，Insert中数据的值，必须使用%s来表示，不需要%d之类的！
+
+                    <h3> First At All </h3>
+                    <ol> 1，添加新表前一定要先测试，测试成功后再确认添加！ </ol>
+
+                    <h3> Special Sign Notice </h3>
+                    <ol> 1，SQL语句的结尾不要添加分号 ';' ！ </ol>
+                    <ol> 2，MySQL中特殊字符 '#' 尽量去掉！ </ol>
+
+                    <h3> MySQL SQL Syntax </h3>
+                    <ol> 1，用&lt;TABLE_NAME&gt;来代替不确定的表名！ </ol>
+
+                    <h3> Create SQL Syntax </h3>
+                    <ol> 1，Create语句中Oracle中的Number类型，对应MySQL中的Bigint类型！ </ol>
+                    <ol> 2，Create语句必须加上 'if not exists'！ </ol>
+                    <ol> 3，Create必须加上 'ENGINE = MEMORY！'！ </ol>
+
+                    <h3> Insert SQL Syntax </h3>
+                    <ol> 1，Insert中数据的值，必须使用%s来表示，不需要%d之类的！ </ol>
                 """,
                 'fields': ('create', 'drop', 'insert', 'delete')
             }
--- mirror/models.py
+++ mirror/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
-# Create your models here.
+# configs for this model.
 
 
 class GlobalConfig(models.Model):
@@ -32,6 +32,77 @@ class GlobalConfig(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+# table sql when cache table into mysql server,abstract class and subclasses all here
+
+
+class TableSQL(models.Model):
+
+    """SQLs the process need if you want to mirror Target tables."""
+
+    class Meta:
+        abstract = True
+
+    # validators
+
+    oracle_special_sign = RegexValidator(regex=r';', inverse_match=True, message="Please Don't Input signs like ';'")
+    mysql_special_sign = RegexValidator(regex=r';|#', inverse_match=True,
+                                        message="Please Don't Input signs like ';' , '#'")
+    table_name = RegexValidator(regex=r'\<TABLE_NAME\>', message="Please Input <TABLE_NAME> !")
+    if_not_exists = RegexValidator(regex=r'(?i)if not exists', message="Please Input 'IF NOT EXISTS' !")
+    engine_is_memory = RegexValidator(regex=r'(?i)engine.*=.*memory', message="Please Input 'ENGINE = MEMORY' !")
+    placeholder = RegexValidator(regex=r'\%s', message="Please Input %s !")
+
+    # Validators collections
+
+    oracle_sql_validators = [oracle_special_sign]
+    create_sql_validators = [mysql_special_sign, table_name, if_not_exists, engine_is_memory]
+    insert_sql_validators = [mysql_special_sign, table_name, placeholder]
+    drop_delete_validators = [mysql_special_sign, table_name]
+
+    # fields
+
+    name = models.CharField(max_length=30, unique=True)
+    enable = models.BooleanField(default=True)
+    desc = models.CharField(max_length=300)
+    period = models.DurationField(help_text="Format is like 0:01:10 for 70 seconds.")
+
+    pull = models.TextField(max_length=900, validators=oracle_sql_validators)
+    create = models.TextField(max_length=900, validators=create_sql_validators)
+    drop = models.TextField(max_length=900, validators=drop_delete_validators)
+    insert = models.TextField(max_length=900, validators=insert_sql_validators)
+    delete = models.TextField(max_length=900, validators=drop_delete_validators)
+
+    # functions
+
+    def apply(self, name):
+
+        """Replace name to <TABLE_NAME> in cache sql statements."""
+
+        self.create = self.create.replace("<TABLE_NAME>", name)
+        self.drop = self.drop.replace("<TABLE_NAME>", name)
+        self.insert = self.insert.replace("<TABLE_NAME>", name)
+        self.delete = self.delete.replace("<TABLE_NAME>", name)
+
+    @classmethod
+    def subclass_name(cls):
+        return cls._meta.verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Ora11gR2(TableSQL):
+
+    """Tables of Oracle whose version is 11gR2."""
+
+    class Meta:
+        verbose_name = "Oracle 11G R2 table sql"
+        verbose_name_plural = "Oracle 11G R2 table sqls"
+
+
+# servers for redis and mysql
 
 
 class RedisServer(models.Model):
@@ -85,10 +156,11 @@ class OracleTarget(models.Model):
         verbose_name = "Oracle Target"
         verbose_name_plural = "Oracle Targets"
 
-    TableSQLChoices = (
-        ('Ora11gR2', 'Oracle 11G R2'),
-    )
+    # choices
+    table_sql_list = [(x.__name__, x.subclass_name()) for x in TableSQL.__subclasses__()]
+    TableSQLChoices = tuple(table_sql_list)
 
+    # fields
     name = models.CharField(max_length=30, unique=True)
     enable = models.BooleanField(default=True)
     desc = models.CharField(max_length=300)
@@ -108,64 +180,3 @@ class OracleTarget(models.Model):
 
     def __unicode__(self):
         return self.name
-
-
-class TableSQL(models.Model):
-
-    """SQLs the process need if you want to mirror Target tables."""
-
-    class Meta:
-        abstract = True
-
-    # validators
-
-    oracle_special_sign = RegexValidator(regex=r';', inverse_match=True, message="Please Don't Input signs like ';'")
-    mysql_special_sign = RegexValidator(regex=r';|#', inverse_match=True,
-                                        message="Please Don't Input signs like ';' , '#'")
-    table_name = RegexValidator(regex=r'\<TABLE_NAME\>', message="Please Input <TABLE_NAME> !")
-    if_not_exists = RegexValidator(regex=r'(?i)if not exists', message="Please Input 'IF NOT EXISTS' !")
-    engine_is_memory = RegexValidator(regex=r'(?i)engine.*=.*memory', message="Please Input 'ENGINE = MEMORY' !")
-    placeholder = RegexValidator(regex=r'\%s', message="Please Input %s !")
-
-    # Validators collections
-
-    oracle_sql_validators = [oracle_special_sign]
-    create_sql_validators = [mysql_special_sign, table_name, if_not_exists, engine_is_memory]
-    insert_sql_validators = [mysql_special_sign, table_name, placeholder]
-    drop_delete_validators = [mysql_special_sign, table_name]
-
-    # fields
-
-    name = models.CharField(max_length=30, unique=True)
-    enable = models.BooleanField(default=True)
-    desc = models.CharField(max_length=300)
-    period = models.DurationField(help_text="Format is like 0:01:10 for 70 seconds.")
-
-    pull = models.TextField(max_length=900, validators=oracle_sql_validators)
-    create = models.TextField(max_length=900, validators=create_sql_validators)
-    drop = models.TextField(max_length=900, validators=drop_delete_validators)
-    insert = models.TextField(max_length=900, validators=insert_sql_validators)
-    delete = models.TextField(max_length=900, validators=drop_delete_validators)
-
-    # functions
-
-    def apply(self, name):
-
-        """Replace name to <TABLE_NAME> in cache sql statements."""
-
-        self.create = self.create.replace("<TABLE_NAME>", name)
-        self.drop = self.drop.replace("<TABLE_NAME>", name)
-        self.insert = self.insert.replace("<TABLE_NAME>", name)
-        self.delete = self.delete.replace("<TABLE_NAME>", name)
-
-    def __unicode__(self):
-        return self.name
-
-
-class Ora11gR2(TableSQL):
-
-    """Tables of Oracle whose version is 11gR2."""
-
-    class Meta:
-        verbose_name = "Oracle 11G R2 table sql"
-        verbose_name_plural = "Oracle 11G R2 table sqls"
